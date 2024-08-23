2338111ecee967c39abdbb189266a91fb1e26eb3
Dmytro Lytvynenko <129224195+DmLytvynenko@users.noreply.github.com>
2024-08-23 23:21:36 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function checkEmail(){
+    let email = document.querySelector('#emailField').value;
+    if (!email.includes('@')) alert('No symbol @');
+    else if (!email.includes('.')) alert('No symbol .');
+}
